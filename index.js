88a8dd198c08c7cc1072d319f2e8d4b950b9f3a8
Priyanitrkl <108118598+Priyanitrkl@users.noreply.github.com>
2022-12-09 19:40:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function myScript(){
 var random1 = Math.floor(Math.random()*6)+1;
-var ranimg1 = "images/dice" + random1+".png";
+var ranimg1 = "dice" + random1+".png";
 document.querySelectorAll("img")[0].setAttribute("src", ranimg1);
 var random2 = Math.floor(Math.random()*6)+1;
-var ranimg2 = "images/dice" + random2+".png";
+var ranimg2 = "dice" + random2+".png";
 document.querySelectorAll("img")[1].setAttribute("src", ranimg2);
 if(random1>random2){
   document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
