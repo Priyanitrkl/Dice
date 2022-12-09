158101b839413757d07193bc4377910cd12eb546
Priyanitrkl <108118598+Priyanitrkl@users.noreply.github.com>
2022-12-09 19:37:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function myScript(){
+var random1 = Math.floor(Math.random()*6)+1;
+var ranimg1 = "images/dice" + random1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", ranimg1);
+var random2 = Math.floor(Math.random()*6)+1;
+var ranimg2 = "images/dice" + random2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", ranimg2);
+if(random1>random2){
+  document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+}
+else if (random1<random2){
+   document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+}
+else document.querySelector("h1").innerHTML = "Draw!";
+}
